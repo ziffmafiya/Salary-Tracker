91a2bbb2ae620f7fb1891e91fb0afdb6ab1ce83a
ZiffMafiya <zifmafiya@gmail.com>
2025-07-23 13:16:26 +0300

    w

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // Replace with your actual Supabase URL and Anon Key
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://wksretbvtrcdmznmuofy.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrc3JldGJ2dHJjZG16bm11b2Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNTk2ODMsImV4cCI6MjA2ODgzNTY4M30.A9RJtjQnCqZtWtgkTURN3Wnr1l3uPfDRvOnB8zAQ784';
